8d9e586aaf51f44cb4263b3f40cfcab82f74fd1c
stoney <05kraken@proton.me>
2023-02-02 08:26:51 +0530

    telescope: use pcall on all require

--- lua/configs/telescope/init.lua
+++ lua/configs/telescope/init.lua
@@ -1,80 +1,77 @@
-local status_ok, telescope = pcall(require, "telescope")
-if not status_ok then
-  return
+local telescope_ok, telescope = pcall(require, "telescope")
+local actions_ok, actions = pcall(require, "telescope.actions")
+
+if telescope_ok and actions_ok then
+  telescope.setup({
+    defaults = {
+      color_devicons = true,
+      sorting_strategy = "ascending",
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+      },
+      layout_config = {
+        width = 0.75,
+        height = 0.65,
+        prompt_position = "top",
+        horizontal = {
+          preview_width = function(_, cols, _)
+            if cols > 200 then
+              return math.floor(cols * 0.4)
+            else
+              return math.floor(cols * 0.6)
+            end
+          end,
+        },
+        vertical = { width = 0.9, height = 0.95, preview_height = 0.5 },
+        flex = { horizontal = { preview_width = 0.9 } },
+      },
+      mappings = {
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-d>"] = actions.delete_buffer,
+          ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+          ["<CR>"] = actions.select_default,
+        },
+        n = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-d>"] = actions.delete_buffer,
+          ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+        },
+      },
+    },
+    pickers = {
+      buffers = {
+        show_all_buffers = true,
+        sort_mru = true,
+        previewer = false,
+      },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      },
+      file_browser = {
+        theme = "ivy",
+        hijack_netrw = false,
+        files = false,
+      },
+    },
+  })
+
+  -- Extensions for telescope
+  telescope.load_extension("fzf")
+  telescope.load_extension("file_browser")
+  telescope.load_extension("persisted") -- To load the telescope extension
+
 end
-
--- Telescope
-local actions = require("telescope.actions")
--- local builtin = require("telescope.builtin")
-
-telescope.setup({
-  defaults = {
-    color_devicons = true,
-    sorting_strategy = "ascending",
-    vimgrep_arguments = {
-      "rg",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case",
-    },
-    layout_config = {
-      width = 0.75,
-      height = 0.65,
-      prompt_position = "top",
-      horizontal = {
-        preview_width = function(_, cols, _)
-          if cols > 200 then
-            return math.floor(cols * 0.4)
-          else
-            return math.floor(cols * 0.6)
-          end
-        end,
-      },
-      vertical = { width = 0.9, height = 0.95, preview_height = 0.5 },
-      flex = { horizontal = { preview_width = 0.9 } },
-    },
-    mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-d>"] = actions.delete_buffer,
-        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-        ["<CR>"] = actions.select_default,
-      },
-      n = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-d>"] = actions.delete_buffer,
-        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-      },
-    },
-  },
-  pickers = {
-    buffers = {
-      show_all_buffers = true,
-      sort_mru = true,
-      previewer = false,
-    },
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true, -- false will only do exact matching
-      override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-    },
-    file_browser = {
-      theme = "ivy",
-      hijack_netrw = false,
-      files = false,
-    },
-  },
-})
-
--- Extensions for telescope
-require("telescope").load_extension("fzf")
-require("telescope").load_extension("file_browser")
-require("telescope").load_extension("persisted") -- To load the telescope extension
