15cc2a9fc286597765e55ff70cc8088185c275ed
stoney <05kraken@proton.me>
2023-02-02 11:58:58 +0530

    update: Nvimtree

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -1,4 +1,5 @@
 local status_ok, tree = pcall(require, "nvim-tree")
+local icon = require("core.icons")
 if status_ok then
   tree.setup({
     sync_root_with_cwd = true,
@@ -11,6 +12,34 @@ if status_ok then
     },
     git = {
       ignore = false, -- Show files that are not managed by git
+    },
+    renderer = {
+      indent_markers = {
+        enable = true,
+        inline_arrows = true,
+        icons = {
+          corner = "└",
+          edge = "│",
+          item = "│",
+          bottom = "─",
+          none = " ",
+        },
+      },
+      icons = {
+        glyphs = {
+          default = icon.kind.File,
+          symlink = icon.ui.FileSymlink,
+          bookmark = icon.ui.BookMark,
+          modified = icon.ui.Circle,
+          git = {
+            unstaged = icon.git.FileUnstaged,
+            staged = icon.git.FileStaged,
+            unmerged = icon.git.FileUnmerged,
+            renamed = icon.git.FileRenamed,
+            untracked = icon.git.FileUntracked,
+          },
+        },
+      },
     },
   })
 end
