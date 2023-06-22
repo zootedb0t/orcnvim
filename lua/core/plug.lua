47d3d9f9b159e961ea5b568313264ec2b9f94cb5
zootedb0t <05kraken@proton.me>
2023-06-22 15:50:15 +0530

    add plugin(flash.nvim): closes #21

--- lua/core/plug.lua
+++ lua/core/plug.lua
@@ -244,6 +244,29 @@ local plugins = {
       })
     end,
   },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    --@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          -- default options: exact mode, multi window, all directions, with a backdrop
+          require("flash").jump()
+        end,
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+      },
+    },
+  },
 }
 
 local opts = {
