d3aed5c06a7e1c04fdba131b5ba575a14b4ac87d
zootedb0t <05kraken@proton.me>
2023-07-15 11:11:12 +0530

    remove keymapping `K` from handlers.lua as it is default on neovim-nightly update lazy-lock.json

--- lua/configs/lsp/handlers.lua
+++ lua/configs/lsp/handlers.lua
@@ -11,7 +11,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local bufopts = { buffer = bufnr }
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
     vim.keymap.set("n", "<localleader>k", vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set("n", "<localleader>D", vim.lsp.buf.type_definition, bufopts)
