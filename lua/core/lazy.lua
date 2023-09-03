709604e7e49266be9811cc7942d7a277ef964ba8
zootedb0t <05kraken@proton.me>
2023-09-03 18:11:06 +0530

    change auto-pair plugin #24
    update lazy-lock.json

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -127,10 +127,10 @@ local plugins = {
   },
 
   {
-    "echasnovski/mini.pairs",
-    event = { "BufReadPost", "BufNewFile" },
+    "altermo/ultimate-autopair.nvim",
+    event = { "InsertEnter", "CmdlineEnter" },
+    branch = "v0.6",
     opts = {},
-    version = false,
   },
 
   {
